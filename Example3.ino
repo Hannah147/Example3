000cc51abaaed898f4611963f0eb32b189828356
Hannah147 <hannah171998@gmail.com>
2019-02-07 12:37:38 +0000

    Step4

--- Example3.ino
+++ Example3.ino
@@ -1,14 +1,17 @@
 
 // Initial Program for GitHub
 
+#include <Wire.h>
+#include "rgb_lcd.h"
 
+rgb_lcd lcd;
 
 void setup() {
   // put your setup code here, to run once:
   // put your setup code here, to run once:
   // Some Code
   lcd.begin(16, 2);
-  lcd.print("My first Program");
+  lcd.print("My first Program 2");
   delay(2000);
 
 
@@ -16,5 +19,26 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+for (int positionCounter = 0; positionCounter < 13; positionCounter++) {
+        // scroll one position left:
+        lcd.scrollDisplayLeft();
+        // wait a bit:
+        delay(150);
+    }
 
+    for (int positionCounter = 0; positionCounter < 29; positionCounter++) {
+        // scroll one position right:
+        lcd.scrollDisplayRight();
+        // wait a bit:
+        delay(150);
+    }
+
+
+    for (int positionCounter = 0; positionCounter < 16; positionCounter++) {
+        // scroll one position left:
+        lcd.scrollDisplayLeft();
+        // wait a bit:
+        delay(150);
+    }
+    delay(1000);
 }
