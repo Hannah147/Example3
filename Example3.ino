b3fbeab0e0fd60d1aa729f501e49abcef6b139ed
Hannah147 <hannah171998@gmail.com>
2019-02-07 12:32:04 +0000

    Step1

---
+++ Example3.ino
@@ -0,0 +1,20 @@
+
+// Initial Program for GitHub
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  // put your setup code here, to run once:
+  // Some Code
+  lcd.begin(16, 2);
+  lcd.print("My first Program");
+  delay(1000);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
